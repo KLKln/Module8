41dfb79162bd4278e906b446cd2ec7df53b24946
Kelly Klein <kmklein2@dmacc.edu>
2020-06-27 15:07:57 -0500

    True test passes

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -1,9 +1,20 @@
-def in_set(a_dict, key):
+def in_dict(a_dict, key):
     """
     use reST style
     :param a_dict:
     :param key:
-    :returns: a boolean based on if key is in the set
+    :returns: a boolean based on if key is in the dictionary
     :raises keyError:
     """
+
+    i_d = True
+    for x in a_dict:
+        if key in a_dict:
+            i_d = True
+            break
+    return i_d
+
+
+if __name__ == '__main__':
+    pass
 pass
