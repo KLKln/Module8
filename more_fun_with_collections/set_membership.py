8f02335e19e7d6ee649e93edaeb5bde376d30822
Kelly Klein <kmklein2@dmacc.edu>
2020-06-27 13:49:44 -0500

    about to test true

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -3,15 +3,23 @@ Program: set_membership.py
 Author:  Kelly Klein
 Date: 06/27/2020
 
-This program .
+This program accept a set and return a boolean value stating
+if the element is in the set.
 """
 
-def in_set():
+
+def in_set(a_set, b_set):
     """
     use reST style
     :param a_set:
-    :param element: element we are chcking if in the set
+    :param element: element we are checking if in the set
     :returns: a boolean on if element is in the set
     :raises keyError:
     """
-    pass
+    i_s = True
+    for x in a_set:
+        if x in a_set:
+            return True
+        
+
+
--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -16,19 +16,19 @@ class MyTestCase(unittest.TestCase):
         #create a set as a variable
         a_set = [1, 3, 5, 7, 9]
         #create an element as a variable
-        element = [3, 5]
+        a_element = [3, 5]
         #assertTrue, expect  True to be the result
         #assertTrue(function_name(parameter11, parameter2)
-        self.assertTrue(sm(a_set), element)
+        self.assertTrue(sm.in_set(a_set), a_element)
 
     def test__in_set_false(self):
         #create a set as a variable
-        a_set = [1, 3, 5, 7, 9]
+        b_set = [1, 3, 5, 7, 9]
         #create an element as a variable
-        element =[2, 4]
+        b_element =[2, 4]
         #assertFalse, expect False to be the result
         #assertFalse(function_name(parameter11, parameter2)
-        self.assertFalse(sm(a_set), element)
+        self.assertFalse(sm.in_set(b_set), b_element)
 
 
 if __name__ == '__main__':
