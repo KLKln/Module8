da32b7dd5ad4f6ed92c7edd7a9cdbed7ce7c1624
Kelly Klein <kmklein2@dmacc.edu>
2020-06-28 16:07:46 -0500

    Update validate_input_in_functions_dict.py

--- more_fun_with_collections/validate_input_in_functions_dict.py
+++ more_fun_with_collections/validate_input_in_functions_dict.py
@@ -6,7 +6,7 @@ This program will .
 """
 
 
-def get_test_scores(invalid_message='Invalid test score, try again!'):
+def get_test_scores():
     """
 Use reST style.
 :param invalid_message:"invalid test score, try again!"
@@ -17,30 +17,31 @@ raises keyError: raises an exception
     scores_dict = dict()
     num_scores = input('How many test scores do you have?')
     # loop to get num_scores input
-    num_scores = int(num_scores)
     while True:
         try:
             x = 1
-            if x <= num_scores:
+            while x <= int(num_scores):
                 score = input('Please enter test score: ')
-                if score.isdigit and 0 <= int(score) <= 100:
-                    scores_dict.update({'test' + str(x): int(score)})
+                if score.isalpha():
+                    raise TypeError
+                if 0 <= int(score) <= 100:
+                    dictionary_update = {'test' + str(x): int(score)}
+                    if str(x) not in scores_dict.keys():
+                        scores_dict.update(dictionary_update)
                     x += 1
-                    break
-        except ValueError:
-            raise ValueError(invalid_message)
+                else: raise TypeError
+        except TypeError:
+            print("positive numbers only please")
             continue
 
     # return scores_dict
-            for value in scores_dict.values():
-                return print(value)
+        for key, value in scores_dict.items():
+            return print(key,':',value)
 
 
 
 # Key valuepair, score is going to be the value
 # I define the key
-
-
 def average_scores(scores_dict):
     # num_scores = get length of dictionary
     num_scores = len(scores_dict)
