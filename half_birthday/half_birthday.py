d5df5fdf20dabedd7b0343ac472b4dff15179073
Kelly Klein <kmklein2@dmacc.edu>
2020-06-28 19:59:28 -0500

    inital commit of half_birthday

---
+++ half_birthday/half_birthday.py
@@ -0,0 +1,14 @@
+import datetime
+"""
+Program: half_birthday.py
+Author: Kelly Klein
+Last date modified: 6/28/2020
+This program will .
+"""
+def half_birthday(input_datetime):
+    """
+Use reST style.
+:param input_datetime
+:return:
+raises keyError: raises an exception
+    """
